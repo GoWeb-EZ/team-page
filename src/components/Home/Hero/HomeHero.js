9a9f05124ea29d2e26c2b3d0b23be396c4d07cfd
shinplest <shinplest@gmail.com>
2024-04-05 02:13:10 +0900

    Remove useless codes

--- src/components/Home/Hero/HomeHero.js
+++ src/components/Home/Hero/HomeHero.js
@@ -4,12 +4,10 @@ import emoji from './emoji.png';
 
 export const HomeHero = () => (
   <s.Root>
-    <s.Background src={bg} width="100%" />
+    <s.Background src={bg} width="100%" alt="background" />
     <s.Body>
       <s.Title>GoWebEZ is Crazy Bastart Team</s.Title>
-      <s.Cta type="button" className="btn btn-outline-dark">
-        Learn more team
-      </s.Cta>
+      <s.Cta type="button">Learn more team</s.Cta>
     </s.Body>
     <s.FirstEmoji src={emoji} alt="emoji" />
     <s.SecondEmoji src={emoji} alt="emoji" />
