0b6480e8c9cc95360e62886af8617d9dd88f6698
Yooshin Kim <yusinkim.or@gmail.com>
2024-04-04 16:24:29 +0900

    홈화면 수정

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
     return (
-        <div style={{ padding: 20 }}>
-            <h2>Home View</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adip. </p>
-        </div>
+        <HomeWrapper>
+            <h2>GoWebEZ is Crazy Bastart Team</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                quam velit, vulputate eu pharetra nec, mattis ac neque.
+            </p>
+
+            <button type="button" class="btn btn-outline-dark">
+                Learn more team
+            </button>
+        </HomeWrapper>
     );
 }
+
+const HomeWrapper = styled.div`
+    padding: 4rem 2rem;
+
+    > p {
+        margin: 2rem 0;
+    }
+`;
