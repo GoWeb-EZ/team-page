4df6f0ca66aca0ef37cdb3726628e54488db6c11
Yooshin Kim <yusinkim.or@gmail.com>
2024-04-07 00:13:44 +0900

    퀴즈 기능 구현

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,31 +7,51 @@ import { quizData, quizList } from './QuizData';
 import Popup from './Popup/Popup';
 
 export default function Quiz() {
+  /* Data */
   const [isOpen, setIsOpen] = useState(false);
-  const [quiz, setQuiz] = useState(null);
+  const [quiz, setQuiz] = useState(-1);
+  const [answers, setAnswers] = useState(['?', '?', '?', '?']);
+  const [result, setResult] = useState(0);
 
+  /* QuizData */
   const { slug } = useParams();
-  const post = quizData[slug];
-  if (!post) {
-    return <span> The blog post youve reqeustsed doesn t exist.</span>;
+  const data = quizData[slug];
+  if (!data) {
+    return <s.Wrapper> The member you&apos;ve reqeustsed doesn t exist.</s.Wrapper>;
   }
 
-  /** Funciton */
+  /** Function */
   function handleOnClick(idx) {
-    setQuiz(quizList[idx]);
+    if (answers[idx] !== '?') return;
+
+    setQuiz(idx);
     setIsOpen(true);
   }
 
   function handleOnSelect(idx) {
     setIsOpen(false);
-    console.log(idx);
+
+    const correctAnswer = quizList[quiz].answers[idx];
+    const selectedAnswer = answerList[quiz];
+    const isCorrect = correctAnswer === selectedAnswer;
+    console.log(`정답: ${correctAnswer} vs 선택된 답: ${selectedAnswer} ==> ${isCorrect}`);
+
+    if (isCorrect) setResult(result + 1);
+    else alert(answerList[quiz] + '이 정답입니다!');
+
+    setAnswers((before) => {
+      const after = [...before];
+      after[quiz] = answerList[quiz];
+      return after;
+    });
   }
 
   function handleOnClose() {
     setIsOpen(false);
   }
 
-  const { name, brief, profileImageURL, github } = post;
+  /* QuizData */
+  const { name, brief, profileImageURL, github, answerList } = data || {};
   return (
     <>
       <s.Wrapper>
@@ -43,24 +63,25 @@ export default function Quiz() {
           <s.Name>{name}</s.Name>
           <s.Description>{brief}</s.Description>
           <s.Result>
-            <b>정답 수</b> 0/4
+            <b>정답 수</b> {result}/{quizList.length}
           </s.Result>
 
           {/* QuizBox */}
           {quizList.map(({ question }, idx) => (
             <s.Box key={idx} onClick={() => handleOnClick(idx)}>
               <b>{question}</b>
-              <span>?</span>
+              <span>{answers[idx]}</span>
             </s.Box>
           ))}
         </s.Right>
       </s.Wrapper>
 
+      {/* Popup */}
       {isOpen && (
         <Popup
           name={name}
-          question={quiz.question}
-          answers={quiz.answers}
+          question={quizList[quiz].question}
+          answers={quizList[quiz].answers}
           onSelect={handleOnSelect}
           onClose={handleOnClose}
         />
